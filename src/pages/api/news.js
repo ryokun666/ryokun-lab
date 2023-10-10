cb8eacc86552f0614c8d0da45e4a1eeacab11a33
ryomen666 <113868184+ryomen666@users.noreply.github.com>
2023-10-10 10:01:28 +0900

    タイトルなしの場合のエラーの解決,Notion エラーの時のログ出るようにした。

--- src/pages/api/news.js
+++ src/pages/api/news.js
@@ -17,20 +17,17 @@ export default async function handler(req, res) {
 
     const news = response.results.map((page) => ({
       id: page.id,
-      title: page.properties.title.title[0].text.content,
+      title: page.properties.title?.title[0]?.text?.content || "タイトルなし",
       url: page.properties.url.url,
       created_at: page.properties.create_at.created_time,
       tag: page.properties.tag.multi_select[0]
         ? createTagList(page.properties.tag.multi_select)
         : ["タグなし"],
-      tag_color: page.properties.tag.multi_select[0]
-        ? page.properties.tag.multi_select[0].color
-        : [],
-      // 他のプロパティも必要に応じて追加できます
     }));
 
     res.status(200).json(news);
   } catch (error) {
+    console.error("Notion API Error:", error.message);
     res.status(500).json({ error: error.message });
   }
 }
